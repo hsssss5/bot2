5f2a0ffbae91b6fee85f6c1d3c9eb36d583921d0
m.poludnitcin <m.poludnitcin>
2024-02-04 21:02:33 +0300

    токен

--- bot.py
+++ bot.py
@@ -1,13 +1,15 @@
 import discord
+from dotenv import load_dotenv
 from discord.ext import commands
+from botToken import token
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='>', intents=intents)
+client = discord.Client(intents=intents)
 
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
 
-
-bot.run('MTIwMjI0MTU1MDA1NTM5MTIzMg.G-ZXwL.r7Szcr9zetMzazeXaeIfILIY1ayxlfrGHXUIdI')
+bot.run(token)
